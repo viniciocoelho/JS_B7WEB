d3559ffca9326f15928bcb90e1ec88951d076eab
vinicioCoelho <coelhocvcg@gmail.com>
2020-08-16 22:03:41 -0300

    Formas de output no JS

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('testando JS');
+/*Modos de exibir elementos na tela com o JS*/
+/*Se relaciona apenas com a área da página */
+document.getElementById("titulo").innerHTML="Opa mudei";
+document.write("Algum texto");
+
+/*Se relaciona com a janela do navegador */
+window.alert("mensagem de exemplo");
+
+/*Mostra apenas no console */
+console.log("Aparece so no console")
