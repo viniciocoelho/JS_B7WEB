b86cac7346650b0cb387d1d26c147ab63033c716
Vinicio Coelho <coelhocvcg@gmail.com>
2020-08-29 18:09:42 -0300

    Aprendendo o contens e fazendo menu que  muda de estado

--- script.js
+++ script.js
@@ -166,7 +166,34 @@ let carros = [
 ];
 console.log(carros[2]['nome']);
 console.log(carros[2].nome);
+
+
+Primeira forma de fazer o menu aparecer e sumir
+utilizado junto com o id menu-area.
+
+function menuToogle() {
+  let menuArea = document.getElementById("menu-area");
+
+  if (menuArea.classList.contains("menu-opened") == true) {
+    menuArea.classList.remove("menu-opened");
+  } else {
+    menuArea.classList.add("menu-opened");
+  }
+}
+
+
+
+Segunda forma de fazer apenas adicionando e retirando
+a width do campo.
+
+
+function menuToogle() {
+  let menuArea = document.getElementById("menu-area");
+
+  if (menuArea.style.width == "200px") {
+    menuArea.style.width = "0px";
+  } else {
+    menuArea.style.width = "200px";
+  }
+}
 */
-
-
-
