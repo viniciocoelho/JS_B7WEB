bb2b2d7eecfe5761562d464a3e0be59444f669c8
Vinicio Coelho <coelhocvcg@gmail.com>
2020-09-01 02:14:07 -0300

    Testando for e while

--- Modulo 2A/script.js
+++ Modulo 2A/script.js
@@ -1,4 +1,4 @@
-let dia = 7;
+/*let dia = 7;
 letdiaNome = "";
 
 switch (dia) {
@@ -13,3 +13,33 @@ switch (dia) {
 }
 
 document.getElementById("dia").innerHTML = "Hoje é " + diaNome;
+
+FOR LOOP
+FOR LOOP ARRAY
+
+let carros = ["ferrari", "fusca", "palio", "corola"];
+
+let html = "<ul>";
+
+for (let i in carros) {
+  html += "<li>" + carros[i] + "</li>";
+}
+
+hmtl = html + "</ul>";
+
+document.getElementById("demo").innerHTML = html;
+*/
+
+let html = "";
+let c = 0;
+
+while (c < 10) {
+  html += "numero: " + c + "<br/>";
+  c++;
+}
+
+for (let c = 1; c <= 10; c++) {
+  html += "Numero: " + c + "<br/>";
+}
+
+document.getElementById("demo").innerHTML = html;
