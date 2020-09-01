7c21ba826fbc118c3a5442a3a52261f29a568bef
Vinicio Coelho <coelhocvcg@gmail.com>
2020-09-01 20:10:55 -0300

    Alteração da estrutura e manipulando numeros

---
+++ Modulo 3/number/script.js
@@ -0,0 +1,23 @@
+/* let n = 10;
+
+let res = n.toString() + 5;
+
+console.log(res); 
+#TOFIXED - define ate qual casa decimal vai pegar
+
+
+let n = 10.85868604304;
+
+let res = "R$ " + n.toFixed(2);
+
+console.log(res);
+
+# PARSEINT - transforma uma string em um número
+# PARSEFLOAT - não arredonda e mantem o valor do numero
+*/
+
+let n = "25";
+
+let res = parseInt(n) + 5;
+
+console.log(res);
