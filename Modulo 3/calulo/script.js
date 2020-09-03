9bedb8ee87c5a1e6e361bdbfb4a040cd4e892dea
vinicioCoelho <coelhocvcg@gmail.com>
2020-09-03 12:38:29 -0300

    calulo com JS

---
+++ Modulo 3/calulo/script.js
@@ -0,0 +1,12 @@
+let novoValor = Math.PI;
+let novoValor = Math.round(3.5); //arredonda 
+let novoValor = Math.floor(3.9); // arredonda pra baixo
+let novoValor = Math.ceil(3.9); // arredonda pra cima
+let novoValor = Math.abs( - 3.96548); // retorna o nr absoluto
+let novoValor = Math.min( 7, 100, 600, 20, 3); // retorna o nr menor
+let novoValor = Math.max( 7, 100, 600, 20, 3); // retorna o nr maior
+let novoValor = Math.random(); // retorna o nr aleatorio entre 0 e 1
+let novoValor = Math.floor (Math.random() * 100); // retorna o nr aleatorio entre 0 e qualquer limite q eu colocar
+
+
+console.log( novoValor);
