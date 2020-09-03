16fc9a8417415204dae27c92b71371586a7ce0f7
vinicioCoelho <coelhocvcg@gmail.com>
2020-09-03 14:36:02 -0300

    Usando template string

---
+++ Modulo 3/templateString/script.js
@@ -0,0 +1,8 @@
+let nome = ' coelho';
+let idade = 90;
+
+/* let frase = ' Meu nome é '+nome+' e eu tenho '+ idade+' anos'; */
+let frase = `Meu nome é ${nome} e eu tenho ${idade} anos e ano que vem vou fazer ${idade+1} anos`;
+
+
+console.log(frase);
